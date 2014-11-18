824880342270cdc426715388d6e9e956f90c181d
Michael Andrea <michaelh.andrea@gmail.com>
2014-11-18 12:06:59 -0800

    Final Maps demo save

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
 		var map = new google.maps.Map(elem, {
 			center: center,
 			zoom: zoom,
-			mapTypeId: google.maps.MapTypeId.SATELLITE
 		});
 
 		var marker = new google.maps.Marker({
@@ -22,6 +21,13 @@ $(document).ready(function() {
 			animation: google.maps.Animation.DROP
 		});
 	
+		var info_win = new google.maps.InfoWindow();
+		info_win.setContent("<h2>Here I Am!</h2><p>Don't you wish you were here?</p>");
+
+		google.maps.event.addListener(marker, 'click', function() {
+			console.log('marker was clicked');
+			info_win.open(map, marker);
+		});
 	} 
 
 	var center = {
@@ -29,5 +35,27 @@ $(document).ready(function() {
 		lng: -122.3080
 	};
 
-	create_map(map_elem, center, 14);
+	function on_success(pos) {
+		var cur_pos = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		};
+
+		create_map(map_elem, cur_pos, 16);
+	}
+
+	function on_error(error) {
+		console.log(error);
+		create_map(map_elem, center, 14);
+	}
+
+	if (navigator && navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(on_success, on_error, {
+			enableHighAccuracy: true,
+			maximumAge: 100000
+		});
+	}
+	else {
+		create_map(map_elem, center, 14);
+	}
 });
